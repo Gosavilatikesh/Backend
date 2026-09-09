af096e6627998178dcc40749c901e5fdbb4bc02b
Gosavilatikesh <gosavilatikesh18@gmail.com>
2026-09-10 00:48:28 +0530

    "Day-11"

---
+++ day-11-access-referesh-token/src/config/db.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+import config from "./config.js"
+
+async function connectDb() {
+    await mongoose.connect(config.MONGO_URI)
+    console.log("Mongo connected");
+    
+}
+
+export default connectDb
